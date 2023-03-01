de1cfd1bfffbff93b8e65382780d7d9d37a65ac3
Popov Ivan <lord.iv88@gmail.com>
2023-03-01 12:02:15 +0800

    Added homework

---
+++ task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.WriteLine("Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(
+    (
+        num1 != num2 
+        ? $"max={(num1 > num2 ? num1 : num2)} min={(num1 < num2 ? num1 : num2)}" 
+        : "Они равны"
+    )
+);
---
+++ task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Программа, определяющая максимальное число из трёх");
+
+Console.Write("Введите три числа через пробел: ");
+int[] numbers = Console.ReadLine()?.Split(' ').Take(3).Select(int.Parse).ToArray()!;
+
+Console.WriteLine("{0} -> {1}", string.Join(", ", numbers), numbers.Max());
